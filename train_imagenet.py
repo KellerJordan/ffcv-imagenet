19d91e344a75efbe3f6883f17656c85fb0d0ac93
KellerJordan <keller@kellerjordan.com>
2022-10-12 22:17:50 +0000

    slim down

--- train_imagenet.py
+++ train_imagenet.py
@@ -1,8 +1,6 @@
 import torch as ch
 from torch.cuda.amp import GradScaler
 from torch.cuda.amp import autocast
-import torch.nn.functional as F
-import torch.distributed as dist
 ch.backends.cudnn.benchmark = True
 ch.autograd.profiler.emit_nvtx(False)
 ch.autograd.profiler.profile(False)
@@ -23,7 +21,7 @@ from argparse import ArgumentParser
 from fastargs import get_current_config
 from fastargs.decorators import param
 from fastargs import Param, Section
-from fastargs.validation import And, OneOf
+from fastargs.validation import OneOf
 
 from ffcv.pipeline.operation import Operation
 from ffcv.loader import Loader, OrderOption
@@ -33,29 +31,17 @@ from ffcv.fields.rgb_image import CenterCropRGBImageDecoder, \
     RandomResizedCropRGBImageDecoder
 from ffcv.fields.basics import IntDecoder
 
-Section('model', 'model details').params(
-    arch=Param(And(str, OneOf(models.__dir__())), default='resnet18'),
-    pretrained=Param(int, 'is pretrained? (1/0)', default=0)
-)
-
 Section('resolution', 'resolution scheduling').params(
-    min_res=Param(int, 'the minimum (starting) resolution', default=160),
-    max_res=Param(int, 'the maximum (starting) resolution', default=160),
-    end_ramp=Param(int, 'when to stop interpolating resolution', default=0),
-    start_ramp=Param(int, 'when to start interpolating resolution', default=0)
+    res=Param(int, 'the resolution', default=192)
 )
 
 Section('data', 'data related stuff').params(
     train_dataset=Param(str, '.dat file to use for training', required=True),
     val_dataset=Param(str, '.dat file to use for validation', required=True),
     num_workers=Param(int, 'The number of workers', required=True),
-    in_memory=Param(int, 'does the dataset fit in memory? (1/0)', required=True)
 )
 
 Section('lr', 'lr scheduling').params(
-    step_ratio=Param(float, 'learning rate step ratio', default=0.1),
-    step_length=Param(int, 'learning rate step length', default=30),
-    lr_schedule_type=Param(OneOf(['step', 'cyclic']), default='cyclic'),
     lr=Param(float, 'learning rate', default=0.5),
     lr_peak_epoch=Param(int, 'Epoch at which LR peaks', default=2),
 )
@@ -72,21 +58,11 @@ Section('validation', 'Validation parameters stuff').params(
 )
 
 Section('training', 'training hyper param stuff').params(
-    eval_only=Param(int, 'eval only?', default=0),
     batch_size=Param(int, 'The batch size', default=512),
-    optimizer=Param(And(str, OneOf(['sgd'])), 'The optimizer', default='sgd'),
     momentum=Param(float, 'SGD momentum', default=0.9),
     weight_decay=Param(float, 'weight decay', default=4e-5),
     epochs=Param(int, 'number of epochs', default=30),
-    label_smoothing=Param(float, 'label smoothing parameter', default=0.1),
-    distributed=Param(int, 'is distributed?', default=0),
-    use_blurpool=Param(int, 'use blurpool?', default=0)
-)
-
-Section('dist', 'distributed training options').params(
-    world_size=Param(int, 'number gpus', default=1),
-    address=Param(str, 'address', default='localhost'),
-    port=Param(str, 'port', default='12355')
+    runs=Param(int, 'number of runs', default=1),
 )
 
 IMAGENET_MEAN = np.array([0.485, 0.456, 0.406]) * 255
@@ -94,47 +70,18 @@ IMAGENET_STD = np.array([0.229, 0.224, 0.225]) * 255
 DEFAULT_CROP_RATIO = 224/256
 
 @param('lr.lr')
-@param('lr.step_ratio')
-@param('lr.step_length')
 @param('training.epochs')
-def get_step_lr(epoch, lr, step_ratio, step_length, epochs):
-    if epoch >= epochs:
-        return 0
-
-    num_steps = epoch // step_length
-    return step_ratio**num_steps * lr
-
-@param('lr.lr')
-@param('training.epochs')
-@param('lr.lr_peak_epoch')
-def get_cyclic_lr(epoch, lr, epochs, lr_peak_epoch):
-    xs = [0, lr_peak_epoch, epochs]
+def get_cyclic_lr(epoch, lr, epochs):
+    xs = [0, 2, epochs]
     ys = [1e-4 * lr, lr, 0]
     return np.interp([epoch], xs, ys)[0]
 
-class BlurPoolConv2d(ch.nn.Module):
-    def __init__(self, conv):
-        super().__init__()
-        default_filter = ch.tensor([[[[1, 2, 1], [2, 4, 2], [1, 2, 1]]]]) / 16.0
-        filt = default_filter.repeat(conv.in_channels, 1, 1, 1)
-        self.conv = conv
-        self.register_buffer('blur_filter', filt)
-
-    def forward(self, x):
-        blurred = F.conv2d(x, self.blur_filter, stride=1, padding=(1, 1),
-                           groups=self.conv.in_channels, bias=None)
-        return self.conv.forward(blurred)
-
 class ImageNetTrainer:
-    @param('training.distributed')
-    def __init__(self, gpu, distributed):
+    def __init__(self, gpu):
         self.all_params = get_current_config()
         self.gpu = gpu
 
         self.uid = str(uuid4())
-
-        if distributed:
-            self.setup_distributed()
 
         self.train_loader = self.create_train_loader()
         self.val_loader = self.create_val_loader()
@@ -142,55 +89,9 @@ class ImageNetTrainer:
         self.create_optimizer()
         self.initialize_logger()
         
-
-    @param('dist.address')
-    @param('dist.port')
-    @param('dist.world_size')
-    def setup_distributed(self, address, port, world_size):
-        os.environ['MASTER_ADDR'] = address
-        os.environ['MASTER_PORT'] = port
-
-        dist.init_process_group("nccl", rank=self.gpu, world_size=world_size)
-        ch.cuda.set_device(self.gpu)
-
-    def cleanup_distributed(self):
-        dist.destroy_process_group()
-
-    @param('lr.lr_schedule_type')
-    def get_lr(self, epoch, lr_schedule_type):
-        lr_schedules = {
-            'cyclic': get_cyclic_lr,
-            'step': get_step_lr
-        }
-
-        return lr_schedules[lr_schedule_type](epoch)
-
-    # resolution tools
-    @param('resolution.min_res')
-    @param('resolution.max_res')
-    @param('resolution.end_ramp')
-    @param('resolution.start_ramp')
-    def get_resolution(self, epoch, min_res, max_res, end_ramp, start_ramp):
-        assert min_res <= max_res
-
-        if epoch <= start_ramp:
-            return min_res
-
-        if epoch >= end_ramp:
-            return max_res
-
-        # otherwise, linearly interpolate to the nearest multiple of 32
-        interp = np.interp([epoch], [start_ramp, end_ramp], [min_res, max_res])
-        final_res = int(np.round(interp[0] / 32)) * 32
-        return final_res
-
     @param('training.momentum')
-    @param('training.optimizer')
     @param('training.weight_decay')
-    @param('training.label_smoothing')
-    def create_optimizer(self, momentum, optimizer, weight_decay,
-                         label_smoothing):
-        assert optimizer == 'sgd'
+    def create_optimizer(self, momentum, weight_decay):
 
         # Only do weight decay on non-batchnorm parameters
         all_params = list(self.model.named_parameters())
@@ -205,20 +106,17 @@ class ImageNetTrainer:
         }]
 
         self.optimizer = ch.optim.SGD(param_groups, lr=1, momentum=momentum)
-        self.loss = ch.nn.CrossEntropyLoss(label_smoothing=label_smoothing)
+        self.loss = ch.nn.CrossEntropyLoss()
 
     @param('data.train_dataset')
     @param('data.num_workers')
     @param('training.batch_size')
-    @param('training.distributed')
-    @param('data.in_memory')
-    def create_train_loader(self, train_dataset, num_workers, batch_size,
-                            distributed, in_memory):
+    @param('resolution.res')
+    def create_train_loader(self, train_dataset, num_workers, batch_size, res):
         this_device = f'cuda:{self.gpu}'
         train_path = Path(train_dataset)
         assert train_path.is_file()
 
-        res = self.get_resolution(epoch=0)
         self.decoder = RandomResizedCropRGBImageDecoder((res, res))
         image_pipeline: List[Operation] = [
             self.decoder,
@@ -236,18 +134,17 @@ class ImageNetTrainer:
             ToDevice(ch.device(this_device), non_blocking=True)
         ]
 
-        order = OrderOption.RANDOM if distributed else OrderOption.QUASI_RANDOM
         loader = Loader(train_dataset,
                         batch_size=batch_size,
                         num_workers=num_workers,
-                        order=order,
-                        os_cache=in_memory,
+                        order=OrderOption.RANDOM,
+                        os_cache=True,
                         drop_last=True,
                         pipelines={
                             'image': image_pipeline,
                             'label': label_pipeline
                         },
-                        distributed=distributed)
+                        distributed=False)
 
         return loader
 
@@ -255,9 +152,8 @@ class ImageNetTrainer:
     @param('data.num_workers')
     @param('validation.batch_size')
     @param('validation.resolution')
-    @param('training.distributed')
     def create_val_loader(self, val_dataset, num_workers, batch_size,
-                          resolution, distributed):
+                          resolution):
         this_device = f'cuda:{self.gpu}'
         val_path = Path(val_dataset)
         assert val_path.is_file()
@@ -288,15 +184,14 @@ class ImageNetTrainer:
                             'image': image_pipeline,
                             'label': label_pipeline
                         },
-                        distributed=distributed)
+                        distributed=False)
         return loader
 
     @param('training.epochs')
     @param('logging.log_level')
     def train(self, epochs, log_level):
+        print('EPOCHS: %d' % epochs)
         for epoch in range(epochs):
-            res = self.get_resolution(epoch)
-            self.decoder.output_size = (res, res)
             train_loss = self.train_loop(epoch)
 
             if log_level > 0:
@@ -304,47 +199,29 @@ class ImageNetTrainer:
                     'train_loss': train_loss,
                     'epoch': epoch
                 }
-
                 self.eval_and_log(extra_dict)
 
         self.eval_and_log({'epoch':epoch})
-        if self.gpu == 0:
-            ch.save(self.model.state_dict(), self.log_folder / 'final_weights.pt')
+        ch.save(self.model.state_dict(), self.log_folder / 'final_weights.pt')
 
     def eval_and_log(self, extra_dict={}):
         start_val = time.time()
         stats = self.val_loop()
         val_time = time.time() - start_val
-        if self.gpu == 0:
-            self.log(dict({
-                'current_lr': self.optimizer.param_groups[0]['lr'],
-                'top_1': stats['top_1'],
-                'top_5': stats['top_5'],
-                'val_time': val_time
-            }, **extra_dict))
+        self.log(dict({
+            'current_lr': self.optimizer.param_groups[0]['lr'],
+            'top_1': stats['top_1'],
+            'top_5': stats['top_5'],
+            'val_time': val_time
+        }, **extra_dict))
 
         return stats
 
-    @param('model.arch')
-    @param('model.pretrained')
-    @param('training.distributed')
-    @param('training.use_blurpool')
-    def create_model_and_scaler(self, arch, pretrained, distributed, use_blurpool):
+    def create_model_and_scaler(self):
         scaler = GradScaler()
-        model = getattr(models, arch)(pretrained=pretrained)
-        def apply_blurpool(mod: ch.nn.Module):
-            for (name, child) in mod.named_children():
-                if isinstance(child, ch.nn.Conv2d) and (np.max(child.stride) > 1 and child.in_channels >= 16): 
-                    setattr(mod, name, BlurPoolConv2d(child))
-                else: apply_blurpool(child)
-        if use_blurpool: apply_blurpool(model)
-
+        model = models.resnet18(pretrained=False)
         model = model.to(memory_format=ch.channels_last)
         model = model.to(self.gpu)
-
-        if distributed:
-            model = ch.nn.parallel.DistributedDataParallel(model, device_ids=[self.gpu])
-
         return model, scaler
 
     @param('logging.log_level')
@@ -353,7 +230,7 @@ class ImageNetTrainer:
         model.train()
         losses = []
 
-        lr_start, lr_end = self.get_lr(epoch), self.get_lr(epoch + 1)
+        lr_start, lr_end = get_cyclic_lr(epoch), get_cyclic_lr(epoch + 1)
         iters = len(self.train_loader)
         lrs = np.interp(np.arange(iters), [0, iters], [lr_start, lr_end])
 
@@ -421,24 +298,22 @@ class ImageNetTrainer:
             'loss': MeanScalarMetric(compute_on_step=False).to(self.gpu)
         }
 
-        if self.gpu == 0:
-            folder = (Path(folder) / str(self.uid)).absolute()
-            folder.mkdir(parents=True)
+        folder = (Path(folder) / str(self.uid)).absolute()
+        folder.mkdir(parents=True)
 
-            self.log_folder = folder
-            self.start_time = time.time()
+        self.log_folder = folder
+        self.start_time = time.time()
 
-            print(f'=> Logging in {self.log_folder}')
-            params = {
-                '.'.join(k): self.all_params[k] for k in self.all_params.entries.keys()
-            }
+        print(f'=> Logging in {self.log_folder}')
+        params = {
+            '.'.join(k): self.all_params[k] for k in self.all_params.entries.keys()
+        }
 
-            with open(folder / 'params.json', 'w+') as handle:
-                json.dump(params, handle)
+        with open(folder / 'params.json', 'w+') as handle:
+            json.dump(params, handle)
 
     def log(self, content):
         print(f'=> Log: {content}')
-        if self.gpu != 0: return
         cur_time = time.time()
         with open(self.log_folder / 'log', 'a+') as fd:
             fd.write(json.dumps({
@@ -447,33 +322,6 @@ class ImageNetTrainer:
                 **content
             }) + '\n')
             fd.flush()
-
-    @classmethod
-    @param('training.distributed')
-    @param('dist.world_size')
-    def launch_from_args(cls, distributed, world_size):
-        if distributed:
-            ch.multiprocessing.spawn(cls._exec_wrapper, nprocs=world_size, join=True)
-        else:
-            cls.exec(0)
-
-    @classmethod
-    def _exec_wrapper(cls, *args, **kwargs):
-        make_config(quiet=True)
-        cls.exec(*args, **kwargs)
-
-    @classmethod
-    @param('training.distributed')
-    @param('training.eval_only')
-    def exec(cls, gpu, distributed, eval_only):
-        trainer = cls(gpu=gpu)
-        if eval_only:
-            trainer.eval_and_log()
-        else:
-            trainer.train()
-
-        if distributed:
-            trainer.cleanup_distributed()
 
 # Utils
 class MeanScalarMetric(torchmetrics.Metric):
@@ -490,6 +338,13 @@ class MeanScalarMetric(torchmetrics.Metric):
     def compute(self):
         return self.sum.float() / self.count
 
+# main to run multiple
+@param('training.runs')
+def main(runs):
+    for _ in range(runs):
+        trainer = ImageNetTrainer(0)
+        trainer.train()
+
 # Running
 def make_config(quiet=False):
     config = get_current_config()
@@ -502,4 +357,5 @@ def make_config(quiet=False):
 
 if __name__ == "__main__":
     make_config()
-    ImageNetTrainer.launch_from_args()
+    main()
+
